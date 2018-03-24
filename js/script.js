7e9651f21109084aa134bfe38be6003e0b49a779
Mateusz Kirmuć <mateusz.kirmuc@gmail.com>
2018-03-24 16:29:49 +0100

    Grab and store table view elements in script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 // ---- VIEW ----
 
 var newGameBtn = document.getElementById('js-newGameButton');
+
 var pickRock = document.getElementById('js-playerPick_rock'),
   pickPaper = document.getElementById('js-playerPick_paper'),
   pickScissors = document.getElementById('js-playerPick_scissors');
+
 var newGameElem = document.getElementById('js-newGameElement'),
   pickElem = document.getElementById('js-playerPickElement'),
   resultsElem = document.getElementById('js-resultsTableElement');
+
+var playerPointsElem = document.getElementById('js-playerPoints'),
+  playerNameElem = document.getElementById('js-playerName'),
+  computerPointsElem = document.getElementById('js-computerPoints');
 
 newGameBtn.addEventListener('click', newGame);
 pickRock.addEventListener('click', function() {
@@ -24,13 +30,13 @@ pickScissors.addEventListener('click', function() {
 // ---- MODEL ----
 
 var gameState = 'notStarted', //started // ended
-player = {
-  name: '',
-  score: 0
-},
-computer = {
-  score: 0
-};
+  player = {
+    name: '',
+    score: 0
+  },
+  computer = {
+    score: 0
+  };
 
 setGameElements();
 
