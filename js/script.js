e2abc843db37e81a308f7a2e03542800910bd87a
Mateusz Kirmuć <mateusz.kirmuc@gmail.com>
2018-03-24 17:02:44 +0100

    Add winner determining logic

--- js/script.js
+++ js/script.js
@@ -97,6 +97,27 @@ function playerPick(playerPick) {
   var computerPick = getComputerPick();
   playerPickElem.innerHTML = playerPick;
   computerPickElem.innerHTML = computerPick;
+  checkRoundWinner(playerPick, computerPick);
+}
+
+function checkRoundWinner(playerPick, computerPick) {
+  playerResultElem.innerHTML = computerResultElem.innerHTML = '';
+  var winnerIs = 'player';
+  if (playerPick == computerPick) {
+    winnerIs = 'noone'; // remis
+  } else if (
+    (computerPick == 'rock' && playerPick == 'scissors') ||
+    (computerPick == 'scissors' && playerPick == 'paper') ||
+    (computerPick == 'paper' && playerPick == 'rock')) {
+    winnerIs = 'computer';
+  }
+  if (winnerIs == 'player') {
+    playerResultElem.innerHTML = "Win!";
+    player.score++;
+  } else if (winnerIs == 'computer') {
+    computerResultElem.innerHTML = "Win!";
+    computer.score++;
+  }
 }
 
 // ---- ! VIEW MODEL ----
