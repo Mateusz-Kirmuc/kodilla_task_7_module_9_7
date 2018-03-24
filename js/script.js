34a63b3e9659d6581cec8fa054d1fa0255bf3dd3
Mateusz Kirmuć <mateusz.kirmuc@gmail.com>
2018-03-24 14:58:12 +0100

    Add listeners to user choice btns

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
+// Nodes
 var newGameBtn = document.getElementById('js-newGameButton');
+var pickRock = document.getElementById('js-playerPick_rock'),
+  pickPaper = document.getElementById('js-playerPick_paper'),
+  pickScissors = document.getElementById('js-playerPick_scissors');
 
 // Event Handlers
 function newGame() {
   console.log("newGame handler works!");
 }
 
+
 newGameBtn.addEventListener('click', newGame);
+pickRock.addEventListener('click', function() { playerPick('rock'); });
+pickPaper.addEventListener('click', function() { playerPick('paper'); });
+pickScissors.addEventListener('click', function() { playerPick('scissors'); });
