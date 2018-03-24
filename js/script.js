f186675c63b2e629d79a646a6b91a3e78f999578
Mateusz Kirmuć <mateusz.kirmuc@gmail.com>
2018-03-24 14:50:24 +0100

    Add listener to new game btn

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var newGameBtn = document.getElementById('js-newGameButton');
+
+// Event Handlers
+function newGame() {
+  console.log("newGame handler works!");
+}
+
+newGameBtn.addEventListener('click', newGame);
